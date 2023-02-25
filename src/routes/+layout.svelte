23287fa493377386b3f8b61cf003832bb2c746c6
ArttX <arttxiam@gmail.com>
2023-02-25 17:35:58 +0200

    Added Layout page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import "../app.css";
+	import NavBar from "../lib/components/Navbar.svelte";
+	import Footer from "../lib/components/Footer.svelte";
+</script>
+
+<NavBar />
+<main class="h-full"><slot /></main>
+<Footer />
