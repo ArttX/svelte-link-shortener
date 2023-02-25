2d1c9158f8da49625882d73642e1a2cc2c9d4e7b
ArttX <arttxiam@gmail.com>
2023-02-25 17:29:08 +0200

    Added Session types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { Session, DefaultUser } from "@auth/core/types";
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -9,4 +11,9 @@ declare global {
 	}
 }
 
-export { };
+type DefaultSessionUser = DefaultUser & { id: string };
+interface SessionWithId extends Session {
+	user: DefaultSessionUser
+}
+
+export { SessionWithId };
